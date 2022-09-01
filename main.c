ff9d3743850e4e548c0d5373289cc0e7ba3745e6
jaytejada2010 <20103300@usc.edu.ph>
2022-09-01 15:38:08 +0800

    commit

--- main.c
+++ main.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+typedef struct frac{
+    int numerator;
+    int denominator;
+}Fraction;
+
+float fracToDec(){
+
+}
+
+Fraction decToFrac(){
+    
+}
+
+void main(){
+    Fraction in, out;
+
+
+}
